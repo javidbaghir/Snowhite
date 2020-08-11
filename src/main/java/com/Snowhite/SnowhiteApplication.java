834214f81d6cfc020ddf27755ab8a20f05e22e4a
javidbaghir <j.baghir01@gmail.com>
2020-08-11 16:52:33 +0400

    Project initialized

---
+++ src/main/java/com/Snowhite/SnowhiteApplication.java
@@ -0,0 +1,13 @@
+package com.Snowhite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowhiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowhiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Snowhite/SnowhiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Snowhite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowhiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
